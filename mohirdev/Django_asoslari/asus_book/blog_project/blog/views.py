ef0c796f5b87aef108173ecc3da3ab2c059888ca
Shohruh Egamov <shohruhwebdev@gmail.com>
2024-10-11 22:37:56 +0300

    Bu mangodb tugab reactni boshlanishi

--- mohirdev/Django_asoslari/asus_book/blog_project/blog/views.py
+++ mohirdev/Django_asoslari/asus_book/blog_project/blog/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+# from msilib.schema import ListView
+from django.views.generic import ListView
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+
+from .models import Blog
+
+# def bloglistview(request):
+#     blogs = Blog.objects.all()
+#     users = User.objects.all()
+#
+#     context = {
+#         "blogs":blogs,
+#         "users":users
+#     }
+#
+#     return render(request, "home.html", context=context)
+#
+#
+# def blogdetailview(request, id):
+#     # Bu yerda eski versiyasida
+#     # try:
+#     #     blog = Blog.objects.get(id=id)
+#     #     context = {
+#     #         'blog':blog
+#     #     }
+#     # except Blog.DoesNotExist:
+#     #     raise Http404('No blog found')
+#
+#     # Bu yerda yangi versiyasi
+#     blog = get_object_or_404(Blog,id=id)
+#     context = {
+#         "blog" : blog
+#     }
+#     return render(request, "blog_detail.html",context=context)
+
+
+# Bu yerda esa CLASS lardan foydalanib yozamiz
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'home.html'
+    context_object_name = "blogs"
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = "blog_detail.html"
